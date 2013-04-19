8a5e732dc484954a28e4a6f64d1dabe0181b6f08
gilsho <gilsho@cs.stanford.edu>
2013-04-18 23:07:00 -0700

    added runcorn shell script

---
+++ runcorn.sh
@@ -0,0 +1,8 @@
+
+for ((i=1; i<=$2; i++))
+do
+	echo python /afs/ir/users/g/i/gilsho/Desktop/Classes/BIOE332/bioe332assign2/assign2.py -c $1 -s $i 
+	python /afs/ir/users/g/i/gilsho/Desktop/Classes/BIOE332/bioe332assign2/assign2.py -c $1 -s $i 
+done
+
+
